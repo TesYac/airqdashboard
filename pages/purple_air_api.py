87ce30b19411107cd0bfc417a4af6c6adc259bdf
TesYac <67443203+TesYac@users.noreply.github.com>
2026-06-01 19:39:38 -0700

    Add files via upload

--- pages/purple_air_api.py
+++ pages/purple_air_api.py
@@ -1,3 +1,13 @@
 import streamlit as st
+import requests
 
 st.title("Download Purple Air Data using an API")
+
+#Get API Key from the user 
+api_key = st.text_input(
+    "Enter your API Key",
+    type="password"
+)
+
+if api_key:
+    st.success("API key received!")
