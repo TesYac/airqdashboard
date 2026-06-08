4fb4b75883fadb08032c4198b53b2f6eaaf982bd
TesYac <67443203+TesYac@users.noreply.github.com>
2026-06-08 16:13:41 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -73,9 +73,9 @@ if uploaded_file is not None:
     for sensor_index, df in df_dict.items():
         st.write(df.head())
 
-    sens_id_temp = st.write(df_main[df_main['location_label'] == 'SAFE_Sandy_Bar_Creek'].deviceID.unique())
+    sens_id_temp =df_main[df_main['location_label'] == 'SAFE_Sandy_Bar_Creek'].deviceID.unique()
     st.write(df_main[df_main['deviceID'] == sens_id_temp[0]].value_counts)
-    st.write(df_main[df_main['deviceID'] == sens_id_temp[1]].value_counts)
+    # st.write(df_main[df_main['deviceID'] == sens_id_temp[1]].value_counts)
     
     #Split Data Frames Using the Dictionary
     # df_blr = df_dict['BLR_Water_Barn']
