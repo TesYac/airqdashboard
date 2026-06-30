a2ae510a43455fec53673382bddac45060cce6d7
TesYac <67443203+TesYac@users.noreply.github.com>
2026-06-30 14:44:21 -0700

    Add files via upload

--- pages/purple_air_api.py
+++ pages/purple_air_api.py
@@ -464,8 +464,10 @@ if missing_sensors:
                 .astimezone(ZoneInfo("America/Los_Angeles")))
                 st.dataframe(df)
                 if(df['location_type'][0] == 0):
+                    df['location_type'] = df['location_type'].astype(str)
                     df['location_type'][0] = 'Outdoors'
                 elif(df['location_type'][0] == 1):
+                    df['location_type'] = df['location_type'].astype(str)
                     df['location_type'][0] = 'Indoors'
             else:
                 for sensor_index, df in result.items():
