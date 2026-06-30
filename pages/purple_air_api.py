b994509129b0c9bc2211122148c18a2ee1eeac24
TesYac <67443203+TesYac@users.noreply.github.com>
2026-06-30 14:23:18 -0700

    Add files via upload

--- pages/purple_air_api.py
+++ pages/purple_air_api.py
@@ -456,7 +456,7 @@ if missing_sensors:
             if len(result) == 1:
                 sensor_index, df = next(iter(result.items()))
                 df['date_created'] = (datetime
-                .fromtimestamp(df['date_created'] / 1000, UTC)
+                .fromtimestamp(df['date_created'], timezone.UTC)
                 .astimezone(ZoneInfo("America/Los_Angeles")))
                 st.dataframe(df)
             else:
